1b9845549c8706a219af02fd35fb02fb2861ae95
Tarcio Passos Freitas <t.passos.2017.2@gmail.com>
2022-03-06 18:14:50 -0300

    Desafio final

---
+++ padroes-projeto-java/src/one/digitalinnovation/gof/singleton/SingletonLazyHolder.java
@@ -0,0 +1,22 @@
+package one.digitalinnovation.gof.singleton;
+
+/**
+ * @see <a href="https://stackoverflow.com/a/24018148">Referencia</a>
+ * 
+ * @author tpfre
+ *
+ */
+public class SingletonLazyHolder {
+
+	private static class InstanceHolder {
+		public static SingletonLazyHolder instancia = new SingletonLazyHolder();
+	}
+	
+	private SingletonLazyHolder() {
+		super();
+	}
+	
+	public static SingletonLazyHolder getInstancia() {
+		return InstanceHolder.instancia;
+	}
+}
